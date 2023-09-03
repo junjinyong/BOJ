2013524e6a3822ee1a87959ec840b147adc57b38
junjinyong <junjinyong@snu.ac.kr>
2023-09-04 07:28:50 +0900

    16455 Quick Select

--- Step-by-step/13/16455.c
+++ Step-by-step/13/16455.c
@@ -4,51 +4,35 @@ inline void swap(int* a, int* b) {
     *b = temp;
 }
 
-inline int minimum(int a, int b) {
-    return a < b ? a : b;
-}
-
-inline int maximum(int a, int b) {
-    return a > b ? a : b;
-}
-
 int kth(int *a, int n, int k) {
-    int start = 0, end = n - 1, key, begin, first, last;
-    while(start < end) {
-        key = a[start + 1000000007 % (end - start + 1)];
-        last = end;
-        begin = start;
-        for(int i = start; i <= end; ++i) {
-            if(a[i] == key) {
-                swap(a + i, a + begin);
-                ++begin;
+    int *p = a, *r = a + n - 1;
+    const int *x = a + k - 1;
+    
+    while(p < r) {
+        const int key = *(p + 1000000007 % (r - p + 1));
+        int *left = p, *right = r, *i = p;
+        
+        while(i <= right) {
+            if(*i > key) {
+                swap(i, right);
+                --right;
+            } else {
+                if(*i < key) {
+                    swap(i, left);
+                    ++left;
+                }
+                ++i;
             }
         }
-        first = begin;
-        while(first < last) {
-            while(first < last && a[first] < key) {
-                ++first;
-            }
-            while(first < last && a[last] > key) {
-                --last;
-            }
-            swap(a + first, a + last);
-        }
-        if(a[first] < key) {
-            ++first;
-        }
-        for(int i = start, j = maximum(begin, first + start - begin); j < first; ++i, ++j) {
-            a[i] = a[j];
-            a[j] = key;
-        }
-        begin = first + start - begin;
-        if(k <= begin) {
-            end = maximum(start, begin - 1);
-        } else if(k > first) {
-            start = minimum(end, first);
+        
+        if(x < left) {
+            r = left - 1;
+        } else if(x > right) {
+            p = right + 1;
         } else {
             return key;
         }
     }
-    return a[start];
+    
+    return *p;
 }
